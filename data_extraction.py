1ef61a91fd7aae5edfff853c2f456db8869f1574
Divya <divnor80@mit.edu>
2023-04-16 16:19:19 -0400

    data process script + 4 data points

--- data_extraction.py
+++ data_extraction.py
@@ -1,10 +1,13 @@
 """
 Script to extract data from PDB using data_csvs
 """
-
+import biotite
+import biotite.structure.io.pdb as pdb
 import glob
+import mrcfile
 import os
 import pandas as pd
+import pickle
 import shutil
 import urllib.request
 import requests
@@ -23,7 +26,8 @@ def download_from_csvs(path):
     data_frame = pd.concat(content)
 
     for index,row in tqdm(data_frame.iterrows()):
-        pdb_id, emdb_id, method = row["Entry ID"], row["EMDB Map"], row["Reconstruction Method"]
+        pdb_id, emdb_id_1, method = row["Entry ID"], row["EMDB Map"], row["Reconstruction Method"]
+        emdb_id_2 = emdb_id_1.lower().replace("-","_")
         try:
             os.mkdir(f"data/{pdb_id}")
         except:
@@ -34,23 +38,38 @@ def download_from_csvs(path):
                 pdb_file = f"data/{pdb_id}/{pdb_id}.pdb"
                 urllib.request.urlretrieve(pdb_url, pdb_file)
 
-                api_url = f"https://files.rcsb.org/download/{emdb_id}.map"
+                api_url = f"https://files.rcsb.org/pub/emdb/structures/{emdb_id_1}/map/{emdb_id_2}.map.gz"
                 response = urllib.request.urlopen(api_url)
-                with open(f"data/{pdb_id}/{emdb_id}.map", "wb") as f:
+                with open(f"data/{pdb_id}/{pdb_id}.map", "wb") as f:
                     f.write(response.read())
 
                 print(f"Completed download for {pdb_id}.")
             except Exception as e:
-                print(f"URL not found for {pdb_id, emdb_id}.")
+                print(f"URL not found for {pdb_id}.")
                 print(e)
                 shutil.rmtree(f"data/{pdb_id}", ignore_errors=True)
 
 def save_dictionary(path):
-    files = glob.glob(path + "/*.csv")
+    result = {}
+    for subdir, dirs, files in os.walk(path):
+        if len(files) == 2:
+            pdb_id = files[0][:4]
+            print(pdb_id)
+            metadata = {}
+            with mrcfile.open(f"{path}/{pdb_id}/{pdb_id}.map") as mrc:
+                map_data = mrc.data
+                metadata["3D_map"] = map_data
+            pdb_file = pdb.PDBFile.read(f"{path}/{pdb_id}/{pdb_id}.pdb")
+            structure = pdb_file.get_structure()[0]
+            metadata["structure"] = structure.coord.shape
+            result[pdb_id] = metadata
+
+    with open('processed_dataset.pickle', 'wb') as handle:
+        pickle.dump(result, handle)
 
 if __name__ == "__main__":
     csv_path = "data_csvs"
     data_path = "data"
 
-    download_from_csvs(csv_path)
-    #save_dictionary(data_path)
+    #download_from_csvs(csv_path)
+    save_dictionary(data_path)
